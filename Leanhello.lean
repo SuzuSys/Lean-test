e301b5e8b218308a2211e255b4f6b44bb0fab2ce
SuzuSys <sigmatics@outlook.jp>
2026-04-04 01:53:02 +0900

    feat: prove a second example of quantifiers and equality

--- Leanhello.lean
+++ Leanhello.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `Leanhello` library.
 -- Import modules here that should be built as part of the library.
-import Leanhello.Basic
+import Leanhello.dependent_type_theory
+import Leanhello.quantifiers_and_equality
--- Leanhello/quantifiers_and_equality.lean
+++ Leanhello/quantifiers_and_equality.lean
@@ -11,7 +11,9 @@ example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
       fun y : α => ⟨h.1 y, h.2 y⟩
   ⟩
 
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) :=
+  fun h₁ : (∀ x : α, p x → q x) =>
+    fun h₂ : (∀ x : α, p x) =>
+      fun x : α => (h₁ x) (h₂ x)
 
-
-example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
 example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
